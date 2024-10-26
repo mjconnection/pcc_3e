ee865d198c52e8dc25b5ecc718c3d436ecd559f2
Michael Johnson <michael@mjconnection.com>
2024-10-26 19:25:02 -0400

    Starting Python Coding

---
+++ LearningPython/hello_world.py
@@ -0,0 +1,13 @@
+
+first_name = "michael"
+last_name = "johnson"
+message = f"Hello, {first_name.title()} {last_name.title()} from Python3 Commandline"
+print (message)
+bicycles = ['trek', 'cannondale', 'redline', 'specialized', 'pinarello']
+print(bicycles)
+print(bicycles[0])
+print(bicycles[0].title())
+print(bicycles[-1])
+message = f"My first bicycle was a {bicycles[0].title()}."
+print(message) 
+
